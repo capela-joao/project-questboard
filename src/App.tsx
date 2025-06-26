88d66589b283926d29b7ab507f2d9fdc9a3ebdbf
João Paulo Goulart Cavalheiro <joaopaulo1060@hotmail.com>
2025-06-26 16:38:26 -0300

    Subindo primeiros arquivos

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Login from './pages/Login';
+import Register from './pages/Register';
+function App() {
+  return (
+    <div className="fullscreen-center">
+      {/* <Login/> */}
+      <Register/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+import FormRegisterUser from "../components/FormRegisterUser"
+
+const Register = () => {
+  return (
+    <FormRegisterUser/>
+  )
+}
+
+export default Register
